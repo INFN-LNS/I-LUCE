30839598ced97936ff542be92c7dc8ff1763bdde
Giuseppe Cirrone <cirrone@lns.infn.it>
2021-11-30 18:47:26 +0100

    working on calibration

--- RadiochromicAnalysis/Functions/PlotCalibrationAndFit.m
+++ RadiochromicAnalysis/Functions/PlotCalibrationAndFit.m
@@ -14,6 +14,8 @@ hold(axes1,'on');
 
 % Create plot with two data set: the calibration data and the fit
 %
+% Plot the experimental data
+%
 h1 = plot(xData,yData);
 set(h1, 'LineWidth',5,...
      'MarkerSize',16,...
@@ -43,21 +45,34 @@ P3_high = FitCoefficientBounds(6);
 xDataNew = linspace(min(xData), max(xData));
 
 fit_high = P3_high*xDataNew.^3 + P2_high*xDataNew.^2 + P1_high*xDataNew;
-h3 = plot(xDataNew, fit_high);
+fit_low = P3_low*xDataNew.^3 + P2_low*xDataNew.^2 + P1_low*xDataNew;
+
+h3 = plot(xDataNew, fit_high, 'b');
+h4 = plot(xDataNew, fit_low, 'b');
+set(h3, 'LineWidth',2);
+set(h4, 'LineWidth',2);
+
+x2 = [xDataNew, fliplr(xDataNew)];
+inBetween = [fit_low, fliplr(fit_high)];
+
+fill(x2, inBetween, 'black','FaceAlpha', '0.1');
 
 % Fix only the lower limit 
 %
-xlim([0,inf]);
-ylim([0,inf]);
+%xlim([0,inf]);
+%ylim([0,inf]);
 
 % legend of the plot
 %
-legend1 = legend([h1 h2],{'Calibration data','3rd order polynomial fit'});
+legend1 = legend([h1 h2 h3 h4],{'Calibration data',...
+    '3rd order polynomial fit',...
+    '95$\%$ confidence bound', '95$\%$ confidence bound'});
 legend1.Location = 'northwest';
 legend1.Interpreter = 'latex';
-set(legend1, 'Box', 'on', 'Color', [0.8,0.8,0.8], 'EdgeColor', get(legend1, 'Color' )) ;
-%legend boxoff   
+set(legend1, 'Box', 'on', 'Color', [0.8,0.8,0.8]);
+set(legend1, 'EdgeColor', get(legend1, 'Color' ));
 
+%legend boxoff
 
 box(axes1,'on');
 grid(axes1,'on');
@@ -79,18 +94,15 @@ hold(axes1,'off');
 %
 set(axes1,'FontSize',18, 'LineWidth', 2); 
 
-
-
-% Add the information of the fit result inside the plot
+% Add the information of the fit result in the plot title
 % This shall be moved in the plot header
 %
-txt = strcat('The fit equation is: P3*X^3 + P2*X^2 + P1*X');
+txt = strcat('Fit equation: P3*X^3 + P2*X^2 + P1*X;');
 txt1 = strcat('P3 =  ', num2str(fitresult.P3));
 txt2 = strcat('P2 =  ', num2str(fitresult.P2));
 txt3 = strcat('P1 =  ', num2str(fitresult.P1));
 
-text(0.05, 3, txt,'FontSize',16,'Interpreter','none')
-text(0.05, 3.5, txt1,'FontSize',16,'Interpreter','latex')
-text(0.05, 4.0, txt2,'FontSize',16,'Interpreter','latex')
-text(0.05, 4.5, txt3,'FontSize',16, 'Interpreter','latex')
+% Add the plot title with information on Fit
+%
+title(strcat(txt, txt1,'; ',txt2,'; ',txt3));
 
