2d38c10246b5280e210eb3d4db633d7ccf31275c
Giuseppe Cirrone <cirrone@lns.infn.it>
2021-12-16 03:44:28 +0100

    starting implementation of the dose reading

--- RadiochromicAnalysis/Functions/WriteDataAndFitParameters.m
+++ RadiochromicAnalysis/Functions/WriteDataAndFitParameters.m
@@ -21,20 +21,6 @@ pathForCalibrationCurveAndFitParameters= strcat(path, filenameCalibrationCurveAn
 
 fid = fopen(pathForCalibrationCurveAndFitParameters, 'wt');
 
-% Write the file with the calibration points and FIT results
-%
-% fprintf(fid,'%6s %12s\n','Dose [ Gy ]','Net OD');
-% fprintf(fid, '%f %f\n',[DoseValues; NetOpticalDensities]);
-% fprintf(fid, '%12s\n', '------------------------ ');
-% fprintf(fid, '%12s\n', 'POLYNOMIAL FIT PARAMETERS ');
-% fprintf(fid, '%12s\n', '                   ');
-% fprintf(fid, '%12s\n', 'The FIT formula is: P3*X^3 + P2*X^2 + P1*X');
-% fprintf(fid, '%12s\n', 'Values of the fit parameters:');
-% fprintf(fid, '%5s %5s\n', 'P3= ', num2str(fitresult.P3));
-% fprintf(fid, '%5s %5s\n', 'P2= ', num2str(fitresult.P2));
-% fprintf(fid, '%5s %5s\n', 'P1= ', num2str(fitresult.P1));
-% fclose(fid);
-
 fprintf(fid, '%12s\n', 'POLYNOMIAL FIT PARAMETERS ');
 fprintf(fid, '%12s\n', 'The FIT formula is: P3*X^3 + P2*X^2 + P1*X');
 fprintf(fid, '\n')
@@ -44,8 +30,6 @@ fprintf(fid, '\n')
 fprintf(fid,'%6s %6s\n','Dose [ Gy ]','Net OD');
 fprintf(fid, '%f %f\n',[DoseValues, NetOpticalDensities]'); 
 fprintf(fid, '%3s', 'end');
-
-
 fclose(fid);
 end
 
