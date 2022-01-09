53f2a3e7b91bcfd0f3ff1f75cf2ee1c6968814ca
Giuseppe Cirrone <cirrone@lns.infn.it>
2022-01-09 18:49:36 +0100

    added stopping power values

--- Utils/DoseToFluence.m
+++ Utils/DoseToFluence.m
@@ -8,9 +8,17 @@ Dose = input('Insert a dose value in Gy ');
 
 % Stopping power in MeV*cm2/g
 %
-%StoppingPower = 95.89; %@3.9 MeV  in Water
-StoppingPower = 75.59; %@5.3 MeV  in Water
-StoppingPower = 75.59; %@6.5 MeV  in Water
+% StoppingPower = 95.89; %@3.9 MeV  in Water
+% StoppingPower = 75.59; %@5.3 MeV  in Water
+% StoppingPower = 70.91; %@5.75 MeV  in Water
+% StoppingPower = 54.06; %@8.1 MeV  in Water
+% StoppingPower = 54.01; %@6.5 MeV  in Water
+% StoppingPower = 46.04; %@9.9 MeV  in Water
+% StoppingPower = 40.52; %@11.6 MeV  in Water
+% StoppingPower = 37.19; %@12.9 MeV  in Water
+ StoppingPower = 34.03; %@14.4 MeV  in Water
+
+
 
 % Converting the stopping power in J*cm2/Kg
 %
