1b5b942705f84e57fcbbdcfc7656a96e8d8d0edd
cirrone <cirrone@lns.infn.it>
2022-01-10 18:21:44 +0100

    ho lavorato su dose to fluence

--- Utils/DoseToFluence.m
+++ Utils/DoseToFluence.m
@@ -10,13 +10,13 @@ Dose = input('Insert a dose value in Gy ');
 %
 % StoppingPower = 95.89; %@3.9 MeV  in Water
 % StoppingPower = 75.59; %@5.3 MeV  in Water
-% StoppingPower = 70.91; %@5.75 MeV  in Water
+ StoppingPower = 70.91; %@5.75 MeV  in Water
 % StoppingPower = 54.06; %@8.1 MeV  in Water
 % StoppingPower = 54.01; %@6.5 MeV  in Water
 % StoppingPower = 46.04; %@9.9 MeV  in Water
 % StoppingPower = 40.52; %@11.6 MeV  in Water
 % StoppingPower = 37.19; %@12.9 MeV  in Water
- StoppingPower = 34.03; %@14.4 MeV  in Water
+% StoppingPower = 34.03; %@14.4 MeV  in Water
 
 
 
