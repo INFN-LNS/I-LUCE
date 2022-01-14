6c72e9f43a41f8b156267f80f65f6bb6c4a51625
Giuseppe Cirrone <cirrone@lns.infn.it>
2022-01-14 09:47:41 +0100

    Modified Dose to Fluence

--- Utils/DoseToFluence.m
+++ Utils/DoseToFluence.m
@@ -6,13 +6,20 @@
 %
 Dose = input('Insert a dose value in Gy ');
 
-% Stopping power in MeV*cm2/g
+
+% INPUT
 %
+Target_DetectorDistance = 9.5; % in cm
+ROISurface = 0.006; % in cm2
+
+% Stopping power in MeV*cm2/g
+
+ StoppingPower = 147.0; %@2.21 MeV  in Water
 % StoppingPower = 95.89; %@3.9 MeV  in Water
 % StoppingPower = 75.59; %@5.3 MeV  in Water
- StoppingPower = 70.91; %@5.75 MeV  in Water
+% StoppingPower = 70.91; %@5.75 MeV  in Water
+% StoppingPower = 64.38; %@6.5 MeV  in Water
 % StoppingPower = 54.06; %@8.1 MeV  in Water
-% StoppingPower = 54.01; %@6.5 MeV  in Water
 % StoppingPower = 46.04; %@9.9 MeV  in Water
 % StoppingPower = 40.52; %@11.6 MeV  in Water
 % StoppingPower = 37.19; %@12.9 MeV  in Water
@@ -29,3 +36,16 @@ StoppingPower = StoppingPower*1.60218e-13/1e-3;
 Fluence = Dose/StoppingPower;
 
 disp('The partile fluence is ' + string(Fluence) + ' cm-2')
+
+
+% Calculation of the Fluence in steradiant
+%
+Fluence_sr = Fluence/(ROISurface/(Target_DetectorDistance*Target_DetectorDistance));
+disp('The fluence in steradiant is: ' + string(Fluence_sr));
+
+% Calculation of the Flux in steradiant
+%
+Flux_sr = Fluence_sr*ROISurface;
+disp('The flux in steradiant is: ' + string(Flux_sr));
+
+
